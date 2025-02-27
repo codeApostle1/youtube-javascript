baa5d7f761f0580194dda3f65fe177520f728a58
J-max <joelmtn7@gmail.com>
2025-02-27 16:51:36 +0100

    done

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-let owner 
+const searchInput = document.querySelector('#search-input');
+
+clear = document.querySelector('.clear');
+
+searchBtn = document.querySelector('#search-button');
+
+let searchText ;
+
+searchInput.addEventListener('keyup', (e) => {
+    // console.log(e.key);
+    searchText = searchInput.value;
+
+    if(searchText.length >= 1){
+        clear.style.display = 'block';
+    } else {
+        clear.style.display ='none';
+    }
+
+    // SEARCH WITH ENTER KEY
+
+    if (e.key = 'Enter') {
+        googleSearch();
+    }
+})
+
+function googleSearch() {
+    if(searchText.length >= 1) {
+        let url = `http://google.com/search?q=${searchText}`;
+        window.location = url
+
+    }
+}
+
+clear.onclick = () => {
+    clear.style.display = 'none';
+    searchInput.value = '';
+}
+
+searchBtn.addEventListener('click', googleSearch)
+
+
+
+
